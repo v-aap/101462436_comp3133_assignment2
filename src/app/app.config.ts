71c51aad68c8d1a57c90fdb7d45e7643bc5f460c
v-aap <valeria.arce@georgebrown.ca>
2025-04-04 18:11:38 -0400

    debugging attempts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import { GraphQLModule } from './graphql/graphql.module';
 
 import { routes } from './app.routes';
 
@@ -13,18 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    importProvidersFrom(Apollo),
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: 'http://localhost:5000/graphql',
-          }),
-        };
-      },
-      deps: [HttpLink],
-    }
+    importProvidersFrom(GraphQLModule)
   ]
 };
--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
 
+const uri = 'http://localhost:5000/graphql';
 
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache(),
+  };
+}
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
 })
-export class GraphqlModule { }
+export class GraphQLModule {}
